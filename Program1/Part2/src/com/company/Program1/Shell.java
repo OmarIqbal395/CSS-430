0bfcc81c20c0e5ccbb73c0b7ae076dcfe0611098
Thuan Tran <oda1234@uw.edu>
2017-04-03 12:27:52 -0700

    Starting to do Part 2

---
+++ Program1/Part2/src/com/company/Program1/Shell.java
@@ -0,0 +1,15 @@
+package com.company.Program1;
+
+public class Shell
+{
+
+    public static void main(String[] args)
+
+	{
+		if (args.length == 1)
+		{
+			System.err.println( "No Argument");
+		}
+
+    }
+}
