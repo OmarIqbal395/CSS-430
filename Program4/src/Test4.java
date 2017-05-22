14d60f0cf3e2b98aaa824916f54f60fc971b31ce
Thuan Tran <oda1234@uw.edu>
2017-05-21 23:44:47 -0700

    Revert "Update"
    This reverts commit 7686d9aa34a40a9789d096ea2e192e364c403b6e.

--- Program4/src/Test4.java
+++ Program4/src/Test4.java
@@ -31,7 +31,7 @@ public class Test4 extends Thread
         readTime = 0;
         writeTime = 0;
 
-        numberOfLoop = 400;
+        numberOfLoop = 250;
     }
 
     /**
@@ -172,7 +172,7 @@ public class Test4 extends Thread
         }
         long endTime = new Date().getTime();
         long elapsed = endTime - startTime;
-        writeTime = elapsed / numberOfLoop;
+        writeTime = elapsed / 10;
 
         byte[] empty = new byte[size];
         startTime = new Date().getTime();
@@ -188,7 +188,7 @@ public class Test4 extends Thread
         // Same like above. Calculate average time and check to see if they are equal
         endTime = new Date().getTime();
         elapsed = endTime - startTime;
-        readTime = elapsed / numberOfLoop;
+        readTime = elapsed / 10;
         if (!Arrays.equals(randomData, empty))
         {
             SysLib.cout("read and write are different " + "\n");
@@ -231,7 +231,7 @@ public class Test4 extends Thread
         }
         long endTime = new Date().getTime();
         long elapsed = endTime - startTime;
-        writeTime = elapsed / numberOfLoop;
+        writeTime = elapsed / 20;
 
         byte[] empty = new byte[size];
         startTime = new Date().getTime();
@@ -246,7 +246,7 @@ public class Test4 extends Thread
         }
         endTime = new Date().getTime();
         elapsed = endTime - startTime;
-        readTime = elapsed / numberOfLoop;
+        readTime = elapsed / 20;
         if (!Arrays.equals(randomData, empty))
         {
             SysLib.cout("read and write are different " + "\n");
@@ -264,26 +264,18 @@ public class Test4 extends Thread
         // Fill the array with random bytes
         new Random().nextBytes(randomData);
         long startTime = new Date().getTime();
-        for (int i = 1; i < numberOfLoop; i ++)
+        for (int i = 1; i < numberOfLoop; i = i + 2)
         {
-            // access the block at different head, each block has 100 difference
-            for (int j = 1; j < 900; j = j+100)
-            {
-                helperWrite(j, randomData);
-            }
+            helperWrite(i, randomData);
         }
         long endTime = new Date().getTime();
         long elapsed = endTime - startTime;
         writeTime = elapsed / numberOfLoop;
         byte[] empty = new byte[size];
         startTime = new Date().getTime();
-        for (int i = 1; i < numberOfLoop; i++)
+        for (int i = 1; i < numberOfLoop; i = i + 2)
         {
-            for (int j = 1; j < 900; j = j+100)
-            {
-                helperRead(j, empty);
-            }
-
+            helperRead(i, empty);
         }
         endTime = new Date().getTime();
         elapsed = endTime - startTime;
